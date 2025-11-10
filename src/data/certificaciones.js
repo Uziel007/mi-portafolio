761d63661044a8bae7a48274a7bba87bc5ffc00b
Uziel007 <uzielsanchez007@gmail.com>
2025-11-10 14:38:37 -0600

    imagenes sincronizadas con las certificaciones

--- src/data/certificaciones.js
+++ src/data/certificaciones.js
@@ -2,7 +2,7 @@ export const certificaciones = [
   {
     categoria: 'agile',
     titulo: 'Certificación de SCRUM FUNDAMENTALS (2024)',
-    imagen: '/public/assets/Certificaciones/scrumfundamentals.png',
+    imagen: '/assets/Certificaciones/scrumfundamentals.png',
     descripcion: 'Obtuve la Certificación Scrum Fundamentals, que me proporcionó una base sólida en la metodología ágil Scrum. Aprendí a gestionar proyectos de manera eficiente, trabajando en equipos colaborativos para entregar valor rápido y de forma continua.',
     url: 'https://www.scrumstudy.com/certification/verify?type=SFC&number=1056186',
     stagger: 1,
@@ -12,7 +12,7 @@ export const certificaciones = [
   {
     categoria: 'cloud',
     titulo: 'AWS Educate: Introducción a la Nube 101',
-    imagen: '/public/assets/Certificaciones/aws.png',
+    imagen: '/assets/Certificaciones/aws.png',
     descripcion: 'Insignia emitida por Amazon Web Services (AWS) a través del programa AWS Educate, reconociendo la finalización del curso "Introducción a la Nube 101". Este programa proporciona las bases esenciales sobre el funcionamiento de la computación en la nube, los principales servicios de AWS y las mejores prácticas para la implementación de soluciones escalables y seguras.',
     url: 'https://www.credly.com/badges/630eeb13-b229-4148-8bc9-a4a27d93c217/public_url',
     stagger: 6,
@@ -22,7 +22,7 @@ export const certificaciones = [
   {
     categoria: 'tools',
     titulo: 'Certificación en Google Analytics',
-    imagen: '/public/assets/Certificaciones/GoogleAnalytics.png',
+    imagen: '/assets/Certificaciones/GoogleAnalytics.png',
     descripcion: 'Certificación otorgada por Google que avala conocimientos en análisis de datos web, interpretación de métricas clave y optimización del rendimiento digital. Incluye el uso de informes, segmentación avanzada y configuración de eventos.',
     url: 'https://skillshop.credential.net/b52f8295-7bf4-4760-9dae-c189130d9a9f#acc.jZBeV19Q',
     stagger: 2,
@@ -32,9 +32,9 @@ export const certificaciones = [
   {
     categoria: 'agile',
     titulo: 'Santander Gestión de Proyectos y Fundamentos de Metodología Agile',
-    imagen: '/public/assets/Certificaciones/Gestión de Proyectos y Fundamentos de metodología Agile.png',
+    imagen: '/assets/Certificaciones/Gestión de Proyectos y Fundamentos de metodología Agile.png',
     descripcion: 'Certificación emitida por Santander Open Academy al completar con éxito el curso sobre Gestión de Proyectos y Metodología Agile. Incluye fundamentos de Scrum, gestión adaptable, planificación iterativa y liderazgo de equipos en entornos ágiles.',
-    url: '/public/assets/Certificaciones/descargas/santander.pdf',
+    url: '/assets/Certificaciones/descargas/santander.pdf',
     stagger: 6,
     horas: 8,
     completado: 100
@@ -42,7 +42,7 @@ export const certificaciones = [
   {
     categoria: 'programming',
     titulo: 'Introduction to Python',
-    imagen: '/public/assets/Certificaciones/Introduction to Python Certificate.png',
+    imagen: '/assets/Certificaciones/Introduction to Python Certificate.png',
     descripcion: 'Certificación que valida conocimientos fundamentales en Python, incluyendo variables, estructuras de control, funciones y manipulación de datos. Proporciona una base sólida para el desarrollo de software y análisis de datos.',
     url: 'https://data-flair.training/verify/75B547582A-75AF51C5AE-7591D036BD/',
     stagger: 4,
@@ -52,7 +52,7 @@ export const certificaciones = [
   {
     categoria: 'architecture',
     titulo: 'Introduction to Software Architecture',
-    imagen: '/public/assets/Certificaciones/introductiontosoftwarearchitecture.png',
+    imagen: '/assets/Certificaciones/introductiontosoftwarearchitecture.png',
     descripcion: 'Curso de Introduction to Software Architecture que enseña los fundamentos del diseño y estructura de sistemas de software. Explora patrones arquitectónicos, componentes, dependencias y buenas prácticas para construir aplicaciones escalables, seguras y mantenibles.',
     url: 'https://simpli-web.app.link/e/cAtHmD4IkXb',
     stagger: 5,
@@ -62,9 +62,9 @@ export const certificaciones = [
   {
     categoria: 'architecture',
     titulo: 'Fundamentos de la arquitectura de software y el diseño de sistemas',
-    imagen: '/public/assets/Certificaciones/Fundamentos de la arquitectura de software y el diseño de sistemas.png',
+    imagen: '/assets/Certificaciones/Fundamentos de la arquitectura de software y el diseño de sistemas.png',
     descripcion: 'Certificación obtenida al completar el curso de Fundamentos de la arquitectura de software y el diseño de sistemas, centrado en el diseño estructurado de aplicaciones, patrones arquitectónicos, principios SOLID y buenas prácticas de escalabilidad, mantenibilidad y eficiencia en sistemas de software a si mismo realizar diagramas UML.',
-    url: '/public/assets/Certificaciones/descargas/Fundamentos de la arquitectura de software y el diseño de sistemas.pdf',
+    url: '/assets/Certificaciones/descargas/Fundamentos de la arquitectura de software y el diseño de sistemas.pdf',
     stagger: 6,
     horas: 8,
     completado: 100
@@ -72,7 +72,7 @@ export const certificaciones = [
   {
     categoria: 'cloud',
     titulo: 'Desarrollo en la Nube',
-    imagen: '/public/assets/Certificaciones/Top Cloud Development Coder.png',
+    imagen: '/assets/Certificaciones/Top Cloud Development Coder.png',
     descripcion: 'Insignia otorgada por Credmark por alcanzar un desempeño 10% superior en la evaluación de Desarrollo en la Nube, demostrando conocimientos avanzados en despliegue, infraestructura y servicios cloud modernos.',
     url: 'https://credmark.ai/badge/CM-2511-FBKFWYR',
     stagger: 6,
@@ -82,7 +82,7 @@ export const certificaciones = [
   {
     categoria: 'tools',
     titulo: 'Certificación en Notion',
-    imagen: '/public/assets/Certificaciones/cursodenotioncertificadao.png',
+    imagen: '/assets/Certificaciones/cursodenotioncertificadao.png',
     descripcion: 'Curso completo de Notion, una herramienta todo-en-uno para productividad, organización y gestión de proyectos. Aprende desde los fundamentos hasta el uso de bases de datos, widgets, plantillas personalizadas e inteligencia artificial para optimizar tu flujo de trabajo.',
     url: 'https://claseflix.io/validar-certificado/68e61d70c1ae460015c597c7',
     stagger: 6,
@@ -92,7 +92,7 @@ export const certificaciones = [
   {
     categoria: 'agile',
     titulo: 'KanbanLearn Kanban Awareness Certified',
-    imagen: '/public/assets/Certificaciones/KanbanLearnKanbanAwarenessCertified.png',
+    imagen: '/assets/Certificaciones/KanbanLearnKanbanAwarenessCertified.png',
     descripcion: 'Certificación de Kanban Awareness que valida conocimientos sobre los principios y prácticas fundamentales de Kanban. Incluye gestión visual de tareas, flujo de trabajo eficiente y mejora continua para optimizar procesos en proyectos y equipos ágiles.',
     url: 'https://badges.innovativelearning.eu/badge/17d8d470-f87f-4f72-b97c-4a58996927f4',
     stagger: 1,
@@ -102,7 +102,7 @@ export const certificaciones = [
   {
     categoria: 'marketing',
     titulo: 'Certificación Digital Marketing Fundamentals',
-    imagen: '/public/assets/Certificaciones/marketing.png',
+    imagen: '/assets/Certificaciones/marketing.png',
     descripcion: 'Con la Certificación en Marketing, adquirí habilidades en estrategias digitales, gestión de campañas y análisis de mercado. Aprendí a conectar marcas con su audiencia a través de tácticas efectivas y orientadas a resultados.',
     url: 'https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/DigitalMarketingFundamentals-UzielSanchezMarin-611988.pdf',
     stagger: 3,
@@ -112,7 +112,7 @@ export const certificaciones = [
   {
     categoria: 'programming',
     titulo: 'Escuela de JavaScript 2025 - De cero a Master en JavaScript',
-    imagen: '/public/assets/Certificaciones/Escuela de JavaScript 2025 - De cero a Master en JavaScript.jpg',
+    imagen: '/assets/Certificaciones/Escuela de JavaScript 2025 - De cero a Master en JavaScript.jpg',
     descripcion: 'La Escuela de JavaScript te lleva en una ruta completa para convertirte en FullStack Developer. Aprenderás desde los fundamentos del lenguaje hasta desarrollar sitios web interactivos y trabajar con JavaScript en el backend usando Node.js, Express y MongoDB.',
     url: 'https://www.udemy.com/certificate/UC-c19c8edd-de0c-4ee6-b192-738afacc1ad9/',
     stagger: 8,
@@ -122,7 +122,7 @@ export const certificaciones = [
   {
     categoria: 'database',
     titulo: 'Practical Database Course for Beginners',
-    imagen: '/public/assets/Certificaciones/certificadodatabasecourse6coursesin1.jpg',
+    imagen: '/assets/Certificaciones/certificadodatabasecourse6coursesin1.jpg',
     descripcion: 'He completado este curso sobre bases de datos, donde aprendí a estructurar, gestionar y optimizar información utilizando SQL. También desarrollé habilidades en normalización y consultas avanzadas.',
     url: 'https://www.udemy.com/certificate/UC-65101205-8c2c-453d-a6bb-03d6e2466af5/',
     stagger: 9,
@@ -132,7 +132,7 @@ export const certificaciones = [
   {
     categoria: 'programming',
     titulo: 'PHP Masterclass: Desarrollo de Proyectos Complejos y Avanzados',
-    imagen: '/public/assets/Certificaciones/phpmasterproyectosavanzados.jpg',
+    imagen: '/assets/Certificaciones/phpmasterproyectosavanzados.jpg',
     descripcion: 'Certificación en desarrollo avanzado con PHP, donde trabajé en la creación de aplicaciones web escalables. Aprendí sobre patrones de diseño, seguridad y optimización de código.',
     url: 'https://www.udemy.com/certificate/UC-223436ce-99ef-4fa5-88c4-4c687f9512c1/',
     stagger: 10,
@@ -142,7 +142,7 @@ export const certificaciones = [
   {
     categoria: 'programming',
     titulo: 'Universidad Java - De Cero a Experto (+150 hrs)',
-    imagen: '/public/assets/Certificaciones/Universidad Java - Cero a Experto - Actualizado (+150 hrs).jpg',
+    imagen: '/assets/Certificaciones/Universidad Java - Cero a Experto - Actualizado (+150 hrs).jpg',
     descripcion: 'El curso más completo de Java en Udemy, actualizado a 2025. Con más de 150 horas de contenido, cubre desde los fundamentos hasta tecnologías avanzadas como Spring Boot, Hibernate, Jakarta EE y desarrollo FullStack.',
     url: 'https://www.udemy.com/certificate/UC-b9e3657c-1bdf-4c20-a5c5-afb735076931/',
     stagger: 11,
@@ -152,7 +152,7 @@ export const certificaciones = [
   {
     categoria: 'cloud',
     titulo: 'Programa de Maestría en Arquitectura de la Nube',
-    imagen: '/public/assets/Certificaciones/Introducción a Google Cloud Platform.png',
+    imagen: '/assets/Certificaciones/Introducción a Google Cloud Platform.png',
     descripcion: 'Programa avanzado enfocado en arquitectura de la nube con AWS, Azure y Google Cloud. Incluye acceso a contenido oficial de AWS, cupones de examen para Microsoft Azure y certificación alineada con estándares de arquitecto cloud profesional. Duración: 4 meses — Inicio: 7 de noviembre de 2025',
     url: 'https://simpli-web.app.link/e/XDpnTzgV1Xb',
     stagger: 6,
@@ -162,7 +162,7 @@ export const certificaciones = [
   {
     categoria: 'programming',
     titulo: 'Dominar los conceptos básicos de jQuery',
-    imagen: '/public/assets/Certificaciones/dominajquery.jpg',
+    imagen: '/assets/Certificaciones/dominajquery.jpg',
     descripcion: 'He completado esta certificación en jQuery, donde aprendí a manipular el DOM, crear animaciones, manejar eventos y realizar peticiones AJAX. También reforcé mis habilidades en JavaScript.',
     url: 'https://www.udemy.com/certificate/UC-70e496be-05ff-4552-be0f-8039291c9bca/',
     stagger: 12,
@@ -172,7 +172,7 @@ export const certificaciones = [
   {
     categoria: 'frameworks',
     titulo: 'Learn Spring Modulith: Monolith to Microservices Seamlessly',
-    imagen: '/public/assets/Certificaciones/LearnSpringModulithMonolithtoMicroservicesSeamlessly.jpg',
+    imagen: '/assets/Certificaciones/LearnSpringModulithMonolithtoMicroservicesSeamlessly.jpg',
     descripcion: 'Certificación enfocada en la transformación de arquitecturas monolíticas a microservicios con Spring Modulith y Spring Boot 3. Se abordaron temas como modularización de aplicaciones, integración con Spring Data JPA y despliegue con Docker y Kubernetes.',
     url: 'https://www.udemy.com/certificate/UC-4f4d3aa4-df56-421f-b30e-28ba6d6d6649/',
     stagger: 13,
@@ -182,7 +182,7 @@ export const certificaciones = [
   {
     categoria: 'database',
     titulo: 'JavaFX Database Management System! Database Design In JavaFX',
-    imagen: '/public/assets/Certificaciones/JavaFX Database Management System! Database Design In JavaFX.jpg',
+    imagen: '/assets/Certificaciones/JavaFX Database Management System! Database Design In JavaFX.jpg',
     descripcion: 'Este curso enseña cómo crear un sistema completo de gestión de bases de datos en JavaFX, incluyendo la creación de una página de inicio de sesión, conexión con bases de datos SQLite y la creación de una aplicación independiente.',
     url: 'https://www.udemy.com/certificate/UC-dbd5a0ed-9985-4889-89b7-d70143143514/',
     stagger: 14,
@@ -192,7 +192,7 @@ export const certificaciones = [
   {
     categoria: 'leadership',
     titulo: 'Curso de Gerencia',
-    imagen: '/public/assets/Certificaciones/cursodegerencia.png',
+    imagen: '/assets/Certificaciones/cursodegerencia.png',
     descripcion: 'Curso de Gerencia enfocado en el desarrollo de habilidades de liderazgo, planeación estratégica y toma de decisiones efectivas. Aprenderás a dirigir equipos, optimizar recursos y alcanzar objetivos organizacionales con una visión integral y orientada a resultados.',
     url: 'https://claseflix.io/validar-certificado/68e818b5eed4640015fdbc24',
     stagger: 7,
@@ -202,7 +202,7 @@ export const certificaciones = [
   {
     categoria: 'frameworks',
     titulo: 'Universidad Angular - De Cero a Experto - Actualizado',
-    imagen: '/public/assets/Certificaciones/Universidad Angular - De Cero a Experto - Actualizado.jpg',
+    imagen: '/assets/Certificaciones/Universidad Angular - De Cero a Experto - Actualizado.jpg',
     descripcion: 'Certificación otorgada por Udemy al completar el curso Universidad Angular - De Cero a Experto, enfocado en el desarrollo profesional de aplicaciones web modernas utilizando el framework Angular. Incluye fundamentos sólidos de TypeScript, componentes, servicios, módulos, directivas, enrutamiento avanzado, consumo de APIs REST, autenticación con JWT y despliegue de proyectos productivos. Ideal para consolidar habilidades en el ecosistema frontend con buenas prácticas y arquitectura escalable.',
     url: 'https://www.udemy.com/certificate/UC-95b9eda5-35cb-461a-ab1d-2c928641d7af/',
     stagger: 6,
@@ -212,7 +212,7 @@ export const certificaciones = [
   {
     categoria: 'ai',
     titulo: 'Curso de Certificación Profesional en Ingeniería de IA',
-    imagen: '/public/assets/Certificaciones/CursodeCertificaciónProfesionalenIngenieríadeIA.jpg',
+    imagen: '/assets/Certificaciones/CursodeCertificaciónProfesionalenIngenieríadeIA.jpg',
     descripcion: 'Este curso brinda formación integral en el diseño, desarrollo e implementación de soluciones basadas en Inteligencia Artificial. Los participantes aprenderán a construir modelos de machine learning y deep learning, optimizar algoritmos, gestionar datos y aplicar técnicas avanzadas de IA para resolver problemas reales en distintos sectores.',
     url: 'https://www.udemy.com/certificate/UC-b718fbd5-16eb-4264-a228-d94a55f97b9c/',
     stagger: 14,
