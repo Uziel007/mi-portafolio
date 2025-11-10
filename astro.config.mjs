262d5951d6246dbc79ea536a84cad4a8d82227ba
Uziel007 <uzielsanchez007@gmail.com>
2025-11-10 14:10:05 -0600

    cambios para navegación más fluida.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://mi-portafolio-73v.pages.dev',  // Opcional, pero recomendado
+  site: 'https://mi-portafolio-73v.pages.dev',
+  build: {
+    format: 'directory', // ✅ genera /pagina/index.html → accesible como /pagina/
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
